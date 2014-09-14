206394baa4c2c05d22ae8a712b9e41ea55a279c8
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-09-14 08:03:58 -0400

    wired up the inputs to the D3 chart. made fixtures

---
+++ app/utils/party-lookup.js
@@ -0,0 +1,35 @@
+var partyMap = {
+  socialDemocrat: {
+    abbreviation: 'SD',
+    name: 'Social Democrat',
+    color: '#FB5258'
+  },
+  conservative: {
+    abbreviation: 'C',
+    name: 'Conservative',
+    color: '#777777'
+  },
+  green: {
+    abbreviation: 'G',
+    name: 'Green',
+    color: '#BBDF2A'
+  },
+  nationalist: {
+    abbreviation: 'N',
+    name: 'Nationalist',
+    color: '#46C8B3'
+  },
+  liberal: {
+    abbreviation: 'L',
+    name: 'liberal',
+    color: '#F8DB3B',
+  }
+};
+
+export default function lookupType(key, type) {
+  if (typeof partyMap[key] === 'undefined') {
+    return;
+  }
+
+  return partyMap[key][type];
+}
