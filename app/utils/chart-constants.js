b196e6f98711305cc8774643fae22ff77835223b
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-10-05 12:03:01 -0400

    annotation tweaks

--- app/utils/chart-constants.js
+++ app/utils/chart-constants.js
@@ -1,7 +1,7 @@
 export default function() {
   return {
-    height: 450,
-    donutMargin: 40,
+    height: 490,
+    donutMargin: 60,
     donutThickness: 87,
     textOffset: 14,
     transitionDurationMs: 750
