3b9fa3051a44fde7a26d960609c6c4697eef07f2
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-11-01 14:07:55 -0400

    updated to ember CLI 0.1.2

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
 var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
